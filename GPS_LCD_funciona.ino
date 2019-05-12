c5e4e94c1a29aa5716f04f4ae0ed6d265baac195
gevidal1 <50307386+gevidal1@users.noreply.github.com>
2019-05-12 16:15:02 -0400

    Add files via upload

---
+++ GPS_LCD_funciona.ino
@@ -0,0 +1,101 @@
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);        // select the pins used on the LCD panel
+unsigned long tepTimer ;
+
+SoftwareSerial mySerial(13,12);//tx,rx
+TinyGPS gps;
+
+void gpsdump(TinyGPS &gps);
+void printFloat(double f, int digits = 2);
+  long lat, lon;
+  float flat, flon;
+  unsigned long age, date, time, chars;
+  int year;
+  byte month, day, hour, minute, second, hundredths;
+void setup()  
+{
+  lcd.begin(16, 2);                       // start the library
+  // Oploen serial communications and wait for port to open:
+  Serial.begin(9600);
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+}
+
+void loop() // run over and over
+{
+  bool newdata = false;
+  unsigned long start = millis();
+  // Every 5 seconds we print an update
+  while (millis() - start < 5000) 
+  {
+    if (mySerial.available()) 
+
+    {
+      char c = mySerial.read();
+      //Serial.print(c);  // uncomment to see raw GPS data
+      if (gps.encode(c)) 
+      {
+        newdata = true;
+        break;  // uncomment to print new data immediately!
+      }
+    }
+  }
+
+  if (newdata) 
+  {
+
+    gpsdump(gps);
+    Serial.println("lat:");printFloat(flat, 5);
+    Serial.println("long:");printFloat(flon, 5);
+    lcd.setCursor(0, 0);                   // set the LCD cursor   position
+    lcd.print("LAT: ");lcd.print(flat, 5);
+    lcd.setCursor(0,1);
+    lcd.print("LONG: ");lcd.print(flon, 5);
+    delay(5000);
+    Serial.println("-------------");
+    Serial.println();
+  }
+
+}
+
+void gpsdump(TinyGPS &gps)
+{
+  gps.f_get_position(&flat, &flon, &age);
+}
+
+void printFloat(double number, int digits)
+{
+  // Handle negative numbers
+  if (number < 0.0) 
+  {
+     Serial.print('-');
+     number = -number;
+  }
+
+  // Round correctly so that print(1.999, 2) prints as "2.00"
+  double rounding = 0.5;
+  for (uint8_t i=0; i<digits; ++i)
+    rounding /= 10.0;
+
+  number += rounding;
+
+  // Extract the integer part of the number and print it
+  unsigned long int_part = (unsigned long)number;
+  double remainder = number - (double)int_part;
+  Serial.print(int_part);
+
+  // Print the decimal point, but only if there are digits beyond
+  if (digits > 0)
+    Serial.print("."); 
+
+  // Extract digits from the remainder one at a time
+  while (digits-- > 0) 
+  {
+    remainder *= 10.0;
+    int toPrint = int(remainder);
+    Serial.print(toPrint);
+    remainder -= toPrint;
+  }
+} 
